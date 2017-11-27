dcd701444d82d5e95d76282069b1bc24bcff6c70
Nathan Floor <nathan.floor@jembi.org>
2017-11-27 15:45:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bittrexbot
+Crypto Currency trader
---
+++ bot.js
@@ -0,0 +1,15 @@
+var bittrex = require('node-bittrex-api');
+bittrex.options({
+	  'apikey' : API_KEY,
+	  'apisecret' : API_SECRET,
+});
+bittrex.getmarketsummaries( function( data, err ) {
+	  if (err) {
+		      return console.error(err);
+		    }
+	  for( var i in data.result ) {
+		      bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
+			            console.log( ticker );
+			          });
+		    }
+});
